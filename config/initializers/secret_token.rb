8836e4a73a8146c20fee42229c7cdcacce50e88f
franza <nico.franza@gmail.com>
2013-01-05 13:48:29 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tauro::Application.config.secret_token = '965e3bad51964451b32f0bc453e43c03254fc87eaf45df3e68511bac820df882becda6d782dadf44e2db752a51fc3d66146c3a48bbb17213742e74c7a72a32cb'
