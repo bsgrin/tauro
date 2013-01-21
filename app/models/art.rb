288e23d92076dcd156c80b85aa2e33be97be0114
franza <nico.franza@gmail.com>
2013-01-21 23:25:54 +0400

    Deleted extra files

--- app/models/art.rb
+++
@@ -1,9 +0,0 @@
-class Art < ActiveRecord::Base
-  belongs_to :gallery
-  attr_accessible :image
-  validates :image, :attachment_presence => true
-  has_attached_file :image, 
-                    :styles => { :thumb => '50x50#' },
-                    :path => ":rails_root/app/assets/images/arts/:style/:filename",
-                    :url => "/assets/arts/:style/:filename"
-end
