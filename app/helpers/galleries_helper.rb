29d6e99a2ffbe51eb69acf1b2f0b620a61e086a2
franza <nico.franza@gmail.com>
2013-01-21 23:34:51 +0400

    Fixed bug when no galleries

--- app/helpers/galleries_helper.rb
+++ app/helpers/galleries_helper.rb
@@ -1,5 +1,6 @@
 module GalleriesHelper
   def get_gallery_arts gallery
+    return [] if !gallery.nil?
     arts = Dir["app/assets/images/galleries/#{gallery.folder}/*"]
     arts.map! { |a| "galleries/#{gallery.folder}/#{a[/[^\/]+?\.\w+?$/]}" }.sort
   end
