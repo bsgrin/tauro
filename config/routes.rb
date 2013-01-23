19ce800e06fb21c6f43fdec029a6e5ae1cba6ad9
franza <nico.franza@gmail.com>
2013-01-24 02:49:19 +0400

    Making ajax work

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Tauro::Application.routes.draw do
-  resources :galleries
+  resources :galleries do
+    member do
+      get 'show'
+    end
+  end
+
   ActiveAdmin.routes(self)
   devise_for :admin_users, ActiveAdmin::Devise.config
   match '/contacts', :to => 'pages#contacts'
