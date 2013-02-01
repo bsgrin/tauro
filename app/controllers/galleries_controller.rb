a0cb1abc3072a7ad790e308894ae434f9c0ff688
franza <nico.franza@gmail.com>
2013-02-02 03:03:23 +0400

    Removed ajax callbacks, added image preloading

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,14 +1,20 @@
 class GalleriesController < ApplicationController
   def index
-    redirect_to :action => 'show', :id => Gallery.all.sample.id
+    get_galleries_and_show :with_id => :random
   end
 
   def show
-    @galleries = Gallery.all
-    @gallery_to_show = Gallery.find(params[:id])
+    get_galleries_and_show :with_id => params[:id]
     respond_to do |format|
       format.html
       format.js
     end
   end
+
+  private
+
+    def get_galleries_and_show params
+      @galleries = Gallery.all
+      @gallery_to_show = params[:with_id] == :random ? Gallery.all.sample : Gallery.find(params[:with_id])
+    end
 end
